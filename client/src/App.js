25aa9bd71ef3cec39334950fa3110e99880e4f8f
Lisa <mclisa00@gmail.com>
2023-04-07 14:18:03 +0100

    User profile added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Layout from "./components/Layout/Layout";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
 import Reset from "./components/User/Reset";
+import Profile from "./components/User/Profile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/reset" element={<Reset />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
