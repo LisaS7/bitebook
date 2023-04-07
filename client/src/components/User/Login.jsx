c9ac13324b688486af8255e844271fbe80c663fe
Lisa <mclisa00@gmail.com>
2023-04-07 12:14:56 +0100

    Firebase functions created. Working on Login component.

---
+++ client/src/components/User/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  auth,
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+} from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { StyledLogin } from "./LoginStyle";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) {
+      // loading screen
+      return;
+    }
+
+    // if (user) navigate("/home");
+  }, [user, loading, navigate]);
+
+  return (
+    <StyledLogin>
+      <section>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email address"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          onClick={() => logInWithEmailAndPassword(email, password)}
+        ></button>
+        <button onClick={signInWithGoogle}>Sign In with Google</button>
+        <Link to="/reset">Forgot Password?</Link>
+      </section>
+    </StyledLogin>
+  );
+}
---
+++ client/src/firebase.jsx
@@ -0,0 +1,104 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvDGNgCOc-THRKGNLFY4MWuDufM3fh4Dk",
+  authDomain: "bitebook-99854.firebaseapp.com",
+  projectId: "bitebook-99854",
+  storageBucket: "bitebook-99854.appspot.com",
+  messagingSenderId: "645161016588",
+  appId: "1:645161016588:web:3396ba2f9fcfca5faa7d29",
+  measurementId: "G-DDJ1T6WRFM",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+async function signInWithGoogle() {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      // if user is not found in db then create a new user
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+}
+
+async function logInWithEmailAndPassword(email, password) {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+}
+
+async function registerWithEmailAndPassword(name, email, password) {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+}
+
+async function sendPasswordReset(email) {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent! Please check your emails.");
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+}
+
+function logout() {
+  signOut(auth);
+}
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
