1ab68de2b69bfbcc02c4c218ba65a36bc4da8105
Lisa <mclisa00@gmail.com>
2023-04-09 20:44:22 +0100

    Added DELETE test

--- server/src/test/java/uk/co/lisasteven/bitebook/BitebookApplicationTests.java
+++ server/src/test/java/uk/co/lisasteven/bitebook/BitebookApplicationTests.java
@@ -1,18 +1,21 @@
 package uk.co.lisasteven.bitebook;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.co.lisasteven.bitebook.food.Food;
 import uk.co.lisasteven.bitebook.food.FoodController;
+import uk.co.lisasteven.bitebook.food.FoodRepository;
 import uk.co.lisasteven.bitebook.food.FoodService;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,6 +29,7 @@ public class BitebookApplicationTests {
 
 	@MockBean
 	private FoodService service;
+
 
 	@Test
 	public void getFoodsShouldReturnFoodList() throws Exception {
@@ -43,6 +47,19 @@ public class BitebookApplicationTests {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(2)))
 				.andExpect(jsonPath("$[0].name").value("banana"));
+	}
+
+	@Test
+	public void deleteFoodShouldRemoveFood() {
+		Food food2 = new Food(
+				"noodles", "carb", "beige", "bland", "soft", "\uD83C\uDF5C", "Loves with sweet chilli sauce"
+		);
+
+		// when
+		service.deleteFood(food2.getId());
+
+		//then
+		Mockito.verify(service, times(1)).deleteFood(food2.getId());
 	}
 
 }
