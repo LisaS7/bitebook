29cbe17dcc128d15b0da860fbae6a232a8839f24
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-25 16:32:28 +0100

    Added category counts to summary box

--- client/src/food/Summary.jsx
+++ client/src/food/Summary.jsx
@@ -3,9 +3,37 @@ import { SummarySection } from "./style";
 import CategoryPie from "./charts";
 
 export default function Summary({ foods }) {
+  const foodCategories = {
+    liked: foods.filter((food) => food.category === "Yes").length,
+    ok: foods.filter(
+      (food) => food.category === "Rarely" || food.category === "Maybe"
+    ).length,
+    unliked: foods.filter((food) => food.category === "No").length,
+    nottried: foods.filter((food) => food.category === "Untested").length,
+    unknown: foods.filter((food) => !food.category || food.category === "None")
+      .length,
+  };
+
   return (
     <SummarySection>
-      <PinkCard>{foods.length} foods</PinkCard>
+      <PinkCard>
+        <h5>{foods.length} foods</h5>
+        <p>
+          <span>{foodCategories.liked}</span> enjoyed
+        </p>
+        <p>
+          <span>{foodCategories.ok}</span> not too bad
+        </p>
+        <p>
+          <span>{foodCategories.unliked}</span> not so good
+        </p>
+        <p>
+          <span>{foodCategories.nottried}</span> still to taste
+        </p>
+        <p>
+          <span>{foodCategories.unknown}</span> to be categorised
+        </p>
+      </PinkCard>
       <CategoryPie />
     </SummarySection>
   );
--- client/src/food/style.jsx
+++ client/src/food/style.jsx
@@ -4,7 +4,29 @@ export const SummarySection = styled.section`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  padding: 3rem;
   background-color: var(--light);
   border-radius: 10px;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+  & h5 {
+    font-weight: 800;
+    text-align: center;
+    letter-spacing: 0.2rem;
+    padding-bottom: 1rem;
+    border-bottom: 3px solid var(--platinum);
+  }
+
+  & p {
+    font-size: 1rem;
+    margin-bottom: 0;
+    margin-left: 0.8rem;
+  }
+
+  & p > span {
+    margin-right: 0.3rem;
+    color: white;
+    font-weight: 800;
+    font-size: 1.2rem;
+  }
 `;
