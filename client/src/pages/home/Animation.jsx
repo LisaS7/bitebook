fa1ef1fd9904a9624990d244be4065989f1b0203
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-04-27 15:24:16 +0100

    Changed timings

--- client/src/pages/home/Animation.jsx
+++ client/src/pages/home/Animation.jsx
@@ -1,12 +1,11 @@
 import { motion } from "framer-motion";
 
 const draw = {
-  hidden: { pathLength: 0, pathSpacing: 0, opacity: 0 },
+  hidden: { pathLength: 0, opacity: 0 },
   visible: (i) => {
     const delay = 2 + i * 0.5;
     return {
       pathLength: 1,
-      pathSpacing: 100,
       opacity: 1,
       transition: {
         pathLength: {
@@ -68,7 +67,7 @@ export function Animation() {
         rx="48.735"
         ry="42.643"
         variants={draw}
-        custom={0}
+        custom={-2}
       />
       <motion.text
         style={{
@@ -80,7 +79,7 @@ export function Animation() {
         }}
         transform="matrix(1, 0, 0, 1, 30.677876, 11.255055)"
         variants={fadeIn}
-        custom={2}
+        custom={0}
       >
         <tspan x="66.022" y="70.08">
           Add{" "}
@@ -98,7 +97,7 @@ export function Animation() {
         }}
         d="M 151.108 98.308 C 158.304 95.66 164.794 90.843 172.252 88.779 C 183.524 85.659 199.206 89.138 209.221 95.047 C 221.508 102.297 228.807 118.209 244.721 118.201 C 250.545 118.198 258.447 118.754 263.861 116.282 C 269.488 113.714 272.942 108.808 279.781 109.604 C 285.6 110.281 287.943 111.433 290.02 116.828 C 290.897 119.109 293.042 125.457 291.032 124.099"
         variants={drawPath}
-        custom={3}
+        custom={1}
       />
       <path
         style={{
@@ -121,7 +120,7 @@ export function Animation() {
         rx="48.735"
         ry="42.643"
         variants={draw}
-        custom={5}
+        custom={2}
       />
       <motion.text
         style={{
@@ -133,7 +132,7 @@ export function Animation() {
         }}
         transform="matrix(1, 0, 0, 1, 274.320801, 59.347355)"
         variants={fadeIn}
-        custom={6}
+        custom={4}
       >
         <tspan x="66.022" y="70.08">
           Log{" "}
@@ -151,7 +150,7 @@ export function Animation() {
         }}
         d="M 390.874 128.087 C 397.948 130.957 404.325 136.18 411.656 138.417 C 422.734 141.8 438.147 138.028 447.989 131.622 C 460.065 123.762 467.239 106.511 482.88 106.52 C 488.604 106.523 496.37 105.92 501.691 108.6 C 507.221 111.384 510.616 116.703 517.337 115.84 C 523.056 115.106 525.359 113.857 527.4 108.008 C 528.263 105.536 530.37 98.654 528.395 100.126"
         variants={drawPath}
-        custom={7}
+        custom={5}
       />
       <path
         style={{
@@ -174,7 +173,7 @@ export function Animation() {
         rx="48.735"
         ry="42.643"
         variants={draw}
-        custom={9}
+        custom={6}
       />
       <motion.text
         style={{
@@ -186,7 +185,7 @@ export function Animation() {
         }}
         transform="matrix(1, 0, 0, 1, 507.872894, 5.88627)"
         variants={fadeIn}
-        custom={10}
+        custom={8}
       >
         <tspan x="66.022" y="70.08">
           View
@@ -204,7 +203,7 @@ export function Animation() {
         }}
         d="M 624.999 81.478 C 631.61 77.606 637.572 70.563 644.424 67.545 C 654.779 62.983 669.187 68.07 678.387 76.71 C 689.674 87.311 696.38 110.577 711 110.565 C 716.35 110.561 723.61 111.374 728.584 107.759 C 733.753 104.004 736.926 96.831 743.21 97.995 C 748.555 98.985 750.708 100.669 752.616 108.557 C 753.422 111.893 755.392 121.174 753.545 119.189"
         variants={drawPath}
-        custom={12}
+        custom={9}
       />
       <path
         style={{
@@ -227,7 +226,7 @@ export function Animation() {
         rx="48.735"
         ry="42.643"
         variants={draw}
-        custom={14}
+        custom={10}
       />
       <motion.text
         style={{
@@ -239,7 +238,7 @@ export function Animation() {
         }}
         transform="matrix(1, 0, 0, 1, 702.475281, 85.866203)"
         variants={fadeIn}
-        custom={15}
+        custom={12}
       >
         <tspan x="66.022" y="70.08">
           Track
