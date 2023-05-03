12dd934551f193705353d3621410eebf08b876d7
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-05-03 11:38:21 +0100

    Aggregated small bar chart by food name and added buttons for top and bottom 10 foods

--- client/src/pages/dashboard/charts/SmallBarChart.jsx
+++ client/src/pages/dashboard/charts/SmallBarChart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BarChart,
   Bar,
@@ -8,45 +8,82 @@ import {
   ResponsiveContainer,
   LabelList,
 } from "recharts";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 import { getAverage } from "../utils";
 import { colours } from "../style";
 
 export default function SmallBarChart({ foodRecords }) {
-  // TODO: limit to 10ish foods
-  const avgList = foodRecords
-    .filter((record) => record.bites.length)
+  const [selection, setSelection] = useState("top");
+
+  const aggBitesByFoodName = [];
+  foodRecords.forEach((record) => {
+    if (record.bites.length) {
+      const index = aggBitesByFoodName.findIndex(
+        (obj) => obj.food.name === record.food.name
+      );
+      if (index < 0) {
+        aggBitesByFoodName.push({ ...record });
+      } else {
+        aggBitesByFoodName[index].bites = aggBitesByFoodName[
+          index
+        ].bites.concat(record.bites);
+      }
+    }
+  });
+
+  const avgList = aggBitesByFoodName
     .map((record) => ({
       name: record.food.name,
       avgRating: getAverage(record.bites, "rating"),
-    }));
+    }))
+    .sort((a, b) => b.avgRating - a.avgRating);
+
+  const selectedFoods =
+    selection === "top" ? avgList.slice(0, 10) : avgList.slice(-10);
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        data={avgList}
-        margin={{
-          top: 10,
-          right: 20,
-          left: 0,
-          bottom: 10,
-        }}
-      >
-        <XAxis tick={false} height={5} />
-        <YAxis
-          label={{
-            value: "Average Rating",
-            angle: -90,
+    <>
+      <ButtonGroup>
+        <Button onClick={() => setSelection("top")}>Top 10</Button>
+        <Button onClick={() => setSelection("bottom")}>Bottom 10</Button>
+      </ButtonGroup>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={selectedFoods}
+          margin={{
+            top: 10,
+            right: 20,
+            left: 0,
+            bottom: 50,
           }}
-          domain={[0, 6]}
-          ticks={[0, 1, 2, 3, 4, 5]}
-        />
-        <Bar dataKey="avgRating">
-          <LabelList dataKey="name" fontSize={13} fill="black" position="top" />
-          {avgList.map((food, index) => (
-            <Cell fill={colours[Math.round(food.avgRating) - 1]} key={index} />
-          ))}
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+        >
+          <XAxis tick={false} height={5} />
+          <YAxis
+            label={{
+              value: "Average Rating",
+              angle: -90,
+            }}
+            domain={[0, 6]}
+            ticks={[0, 1, 2, 3, 4, 5]}
+          />
+          <Bar dataKey="avgRating">
+            <LabelList
+              dataKey="name"
+              fontSize={13}
+              fill="white"
+              position="inside"
+              angle={-90}
+            />
+            {avgList.map((food, index) => (
+              <Cell
+                fill={colours[Math.round(food.avgRating) - 1]}
+                key={index}
+              />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 }
