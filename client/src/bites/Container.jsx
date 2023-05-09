f71ba464cc822578e12d12e6cfb6665c7447b195
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-05-09 11:56:21 +0100

    Style/layout updates

--- client/src/bites/Container.jsx
+++ client/src/bites/Container.jsx
@@ -5,7 +5,6 @@ import TableRow from "./TableRow";
 import InputRow from "./TableInputRow";
 import Summary from "./Summary";
 import { StyledColumnContainer } from "../Layout/global.style";
-import { NotepadBg } from "../Layout/NotepadBg";
 
 export default function BiteContainer() {
   const { activeData } = useSelector((state) => state);
@@ -14,16 +13,14 @@ export default function BiteContainer() {
   return (
     <StyledColumnContainer>
       <Summary records={activeData.foodRecords} />
-      <NotepadBg>
-        <div style={{ width: "70%", margin: "auto" }}>
-          <EditableTable
-            data={activeData.bites}
-            InputRow={InputRow}
-            TableRow={TableRow}
-            dataTemplate={dataTemplate}
-          />
-        </div>
-      </NotepadBg>
+      <div style={{ width: "70%", margin: "auto" }}>
+        <EditableTable
+          data={activeData.bites}
+          InputRow={InputRow}
+          TableRow={TableRow}
+          dataTemplate={dataTemplate}
+        />
+      </div>
     </StyledColumnContainer>
   );
 }
