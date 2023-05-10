5ebfac50e93a763c626752e4837d02dcc415d93d
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-05-10 11:12:52 +0100

    Added foods and bites count

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,35 @@
+import { useSelector } from "react-redux";
 import { Animation } from "./Animation";
+import { HomeContainer, IconContainer } from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBowlRice, faAppleWhole } from "@fortawesome/free-solid-svg-icons";
+
+const iconVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { delay: 8, duration: 3 } },
+};
 
 export default function Home() {
+  const { foods, bites } = useSelector((state) => state);
   return (
-    <div>
+    <HomeContainer>
       <Animation />
-    </div>
+      <IconContainer
+        initial="initial"
+        animate="animate"
+        variants={iconVariants}
+      >
+        <figure>
+          <FontAwesomeIcon icon={faAppleWhole} />
+          <p className="count count-food">{foods.length}</p>
+          <figcaption>Foods</figcaption>
+        </figure>
+        <figure>
+          <FontAwesomeIcon icon={faBowlRice} />
+          <p className="count count-bites">{bites.length}</p>
+          <figcaption>Bites</figcaption>
+        </figure>
+      </IconContainer>
+    </HomeContainer>
   );
 }
--- client/src/pages/home/style.jsx
+++ client/src/pages/home/style.jsx
@@ -1,7 +1,43 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const HomeContainer = styled.div`
-  /* display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 3rem; */
+  display: flex;
+  flex-direction: column;
+`;
+
+export const IconContainer = styled(motion.div)`
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 15rem;
+  color: var(--green);
+
+  & figure {
+    position: relative;
+    width: 220px;
+  }
+
+  & .count {
+    position: absolute;
+    color: var(--contrast);
+    font-size: 5rem;
+  }
+
+  & .count-food {
+    top: 44%;
+    right: 30%;
+  }
+
+  & .count-bites {
+    top: 48%;
+    right: 24%;
+  }
+
+  & svg {
+    vertical-align: bottom;
+  }
+
+  & figcaption {
+    font-size: 5rem;
+  }
 `;
